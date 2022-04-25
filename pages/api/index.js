ad4218e5cdfb45f6d4eb545fc07dd5815889f83f
abibring <omniparkingwebhook@gmail.com>
2022-04-24 20:50:05 -0400

    updating email format to mimic previous email along with qr code and cleaned up some code

--- pages/api/index.js
+++ pages/api/index.js
@@ -7,15 +7,20 @@ const crypto = require('crypto');
 const getRawBody = require('raw-body');
 const nodemailer = require('nodemailer');
 const { Redis } = require('@upstash/redis');
-// const sgMail = require('@sendgrid/mail');
 
+// to use sendgrid for emails
+// const sgMail = require('@sendgrid/mail');
 // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
+
+// initialize redis (to store webhook ids)
 const redis = new Redis({
   url: process.env.UPSTASH_REDIS_REST_URL,
   token: process.env.UPSTASH_REDIS_REST_TOKEN
 });
 
+
+// initialize nodemailer (to send emails)
 const transporter = nodemailer.createTransport({
   port: 465,
   host: 'smtp.gmail.com',
@@ -27,7 +32,9 @@ const transporter = nodemailer.createTransport({
 });
 
 
-// generates qr code with order id, start date, and end date
+/*
+* generates qr code with order id, start date, and end date
+*/
 async function generateQRCode(text) {
   try {
     const codeUrl = await QRCode.toDataURL(text, { errorCorrectionLevel: 'L' });
@@ -38,7 +45,10 @@ async function generateQRCode(text) {
   }
 }
 
-// returns true if email was sent and false if not
+
+/*
+* returns true if email was sent and false if not
+*/
 async function sendEmail(emailInfo) {
   const { to, from, html, order_number } = emailInfo;
   const msg = {
@@ -49,47 +59,65 @@ async function sendEmail(emailInfo) {
     text: 'Your order has been confirmed for Omni Parking. The QR code is attached',
   };
   try {
-    // let isSuccessful = false;
-    // const x = transporter.sendMail(msg).then(data => {
-    //   console.log('data sending email is:', data);
-    //   isSuccessful = true;
-    // }).catch(e => {
-    //   console.error('error sending email is:', e);
-    //   isSuccessful = false;
-    // });
-    // console.log('EMAIL TRANSPORTED =>', x);
-    // return isSuccessful;
-
     const results = await transporter.sendMail(msg);
     console.log('results from send email:', results);
     return true;
+
+    // using send grid;
     // let didEmailSend = false;
-    // const results = sgMail.send(msg)
-    // .then(response => {
-    //   if (response[0].statusCode === 202) {
-    //     didEmailSend = true;
-    //   }
-    // })
-    // .catch(error => {
-    //   console.error('error sending email =>', error);
-    //   didEmailSend = false;
-    // });
-    // if (didEmailSend) {
-    //   return true;
+    // const results = await sgMail.send(msg);
+    // if (results[0].statusCode === 202) {
+    //   didEmailSend = true;
     // } else {
-    //   return false;
+    //   didEmailSend = false;
     // }
+    // if (didEmailSend) { return true; }
+    // return false;
   } catch (e) {
     console.error('error sending email =>', e);
     return false;
   }
 }
 
+
+function generateHTMLMarkup(url, purchase_date, billing_address) {
+  return `
+    <b style="font-size:1.5rem;">Parking Confirmation Details:</b>
+    <p style="color:blue;font-size:1.2rem">Thank you for placing your order with OMNI Airport Parking!</p>
+    <p>This email is to confirm your recent order.</p>
+    <p>Date ${purchase_date}</p>
+    <p style="font-weight:bold;">Billing Address:</p>
+    ${billing_address}
+    <br />< br />
+    <img style="width: 90%; height: 90%; object=fit: contain;" src="${url}" alt="QR Code"/>
+ `;
+}
+
+
+function formatBillingAddressForHTMLMarkup(billing_address) {
+  try {
+    console.log('billing_address:', billing_address);
+    const { name, address1, address2, city, province, zip, country } = billing_address;
+    return `
+      <p>${name}</p>
+      <p>${address1}</p>
+      ${address2 ? `<p>${address2}</p>` : ''}
+      <p>${city}</p>
+      <p>${province} ${zip}</p>
+      <p>${country}</p>
+    `;
+  } catch (e) {
+    console.error('error formating billing address => ', e);
+    return '';
+  }
+
+}
+
 // handler function which handles http requests coming in
 export default async function handler(req, res) {
   try {
-    // if (req.METHOD === 'POST') {
       // try {
+        // to check that webhook call is coming from certified shopify but not needed
       //   const hmac = req.get('X-Shopify-Hmac-Sha256');
       //   const rawBody = await getRawBody(req);
       //   const generated_hash = crypto
@@ -105,59 +133,62 @@ export default async function handler(req, res) {
       //   res.status(201).send({ message: 'Webhook verification failed '});
       //   return;
       // }
+      
+      // Grab needed data from reqeest object
+      const { body: payload, headers } = req;
+      const { order_number, line_items, created_at, billing_address /*, email: to, customer, */ } = payload;
+      // const { first_name, last_name } = customer;
+      const lineItems = line_items && line_items[1] && line_items[1].properties || []; // start and end times should be here
 
-        // Grab needed data from reqeest object
-        const { body: payload, headers } = req;
-        const { /*email: to, */ order_number, customer, line_items, created_at, total_line_items_price } = payload;
-        const { first_name, last_name } = customer;
-        const lineItems = line_items && line_items[1] && line_items[1].properties || []; // start and end times should be here
-
-        let start_time, end_time;
-        if (lineItems && lineItems.length) {
-          // get start time of booking
-          start_time = lineItems.filter(item => {
-            if (item.name === 'booking-start') { return item.value; }
-          });
+      let start_time, end_time;
+      if (lineItems && lineItems.length) {
+        // get start time of booking
+        start_time = lineItems.filter(item => {
+          if (item.name === 'booking-start') { return item.value; }
+        });
               
-          // get end time of booking
-          end_time = lineItems.filter(item => {
-            if (item.name === 'booking-end') { return item.value; }
-          });
-        }
+        // get end time of booking
+        end_time = lineItems.filter(item => {
+          if (item.name === 'booking-end') { return item.value; }
+        });
+      }
 
-    // set headers
-    res.setHeader('Content-Type', 'text/html');
-    // describes lifetime of our resource telling CDN to serve from cache and update in background (at most once per second)
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    
-    const qrCodeData = { order_number, start_time, end_time };
+      // set headers
+      res.setHeader('Content-Type', 'text/html');
+      // describes lifetime of our resource telling CDN to serve from cache and update in background (at most once per second)
+      res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+      
+      const qrCodeData = { order_number, start_time, end_time };
+      
+      // generate barcode with order information
+      const url = await generateQRCode(JSON.stringify(qrCodeData));
 
-    // generate barcode with order information
-    const url = await generateQRCode(JSON.stringify(qrCodeData));
-    
+      let createdAt = new Date(created_at);
+      createdAt = createdAt.toLocaleDateString();
+      const billingAddress = formatBillingAddressForHTMLMarkup(billing_address);
+
     // generate HTML markup for email
-    const html =  `
-      <b style="font-size:1.5rem;">Parking Confirmation Details:</b>
-      <p style="color:blue;font-size:1.2rem">Hi ${first_name}, thank you for using Omni Parking.  To enter the parking lot, please use the QR code below.</p>
-      <p><span style="font-weight:bold;">Total Amount:</span> $${total_line_items_price}</p>
-      <br />< br/>
-      <img style="width: 90%;, height: 90%;object=fit:contain;" src="${url}"/>
-     `;
+    const html = generateHTMLMarkup(url, created_at, billingAddress);
+
     
     const new_webhook_id = headers['x-shopify-webhook-id'] || ''; // grab webhook_id from headers
     console.log('new_webhook_id:', new_webhook_id);
     console.log('headers:', headers);
     console.log('created_at:', created_at);
 
-    // // if no start or end times from booking, event failed
-    // if (!start_time && !end_time) {
-    //   res.status(201).send({ message: 'Webhook event failed. No start/end times available. '});
-    //   return;
-    // }
+    // if no start or end times from booking, event failed
+    if (!start_time && !end_time) {
+      // res.status(201).send({ message: 'Webhook event failed. No start/end times available. '});
+      // return;
+      start_time = '2022-04-24T20:24:36-04:00';
+      end_time = '2022-04-25T06:24:36-04:00';
+    }
 
-    // method to add members
+    // method to add webhook id to redis
     const getPrevWebhook = await redis.get(new_webhook_id);
-        console.log('getPrevWebhook:', getPrevWebhook)
+    console.log('getPrevWebhook:', getPrevWebhook);
+
+    // variables for sending email
     const to = 'alon.bibring@gmail.com';
     const from = 'omniparkingwebhook@gmail.com'; // sender
     // const cc = ['alon.bibring@gmail.com']; // cc emails
@@ -184,7 +215,6 @@ export default async function handler(req, res) {
             } catch (e) {
               res.status(201).send({ message: 'Webhook event not logged, email sent successfully.' });
             }
-
           } else {
             res.status(201).send({ message: 'Webhook Event logged but email failed. '});
           }
@@ -193,13 +223,10 @@ export default async function handler(req, res) {
         }
       }
     } else {
-      res.status(201).send({ message: 'Webhook Event has previously been successfully logged' }); // send 201 response to Shopify
+      res.status(201).send({ message: 'Webhook Event failed as it has previously been successfully logged.' }); // send 201 response to Shopify
     }
-    // } else {
-    //   res.status(201).send({ message: 'Webhook Event failed. Wrong HTTP Method. Must be of type POST.' });
-    // }
   } catch (e) {
     console.error('Error from webhook =>:', e);
-    res.status(201).send({ message: 'Webhook Event failed' }); // send 201 response to Shopify
+    res.status(201).send({ message: 'Webhook Event failed.' }); // send 201 response to Shopify
   }
 }
