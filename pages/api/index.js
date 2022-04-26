5aa1213aef625065840ae1743f1e571fe39932eb
abibring <omniparkingwebhook@gmail.com>
2022-04-26 12:22:45 -0400

    logging response from aws

--- pages/api/index.js
+++ pages/api/index.js
@@ -23,11 +23,6 @@ const s3 = new AWS.S3({
   secretAccessKey: process.env.AMAZ_SECRET_ACCESS_KEY
 });
 
-const params = {
-  Bucket: 'omni-airport-parking',
-  Key: 'omni-airport-parking-logo.png'
-};
-
 // to use sendgrid for emails
 // const sgMail = require('@sendgrid/mail');
 // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -122,11 +117,14 @@ export default async function handler(req, res) {
       const subPrice = subtotal_price || current_subtotal_price;
       const totalTax = total_tax || current_total_tax;
       const totalPrice = total_price || current_total_price;
-
+      const params = {
+        Bucket: 'omni-airport-parking',
+        Key: 'omni-airport-parking-logo.png'
+      };
       const awsResponse = await s3.getObject(params);
-      // console.log('awsResponse:', awsResponse);
-      const imagePath = Buffer.from(awsResponse.Body, 'binary').toString('base64');
-      console.log('imagePath:', imagePath);
+      console.log('awsResponse:', awsResponse);
+      // const imagePath = Buffer.from(awsResponse.Body, 'binary').toString('base64');
+      // console.log('imagePath:', imagePath);
 
       const htmlMarkupData = {
         subtotal_price: subPrice, total_tax: totalTax, total_price: totalPrice,
