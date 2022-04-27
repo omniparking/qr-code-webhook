411ef0e587a288c8d05e936a82d93767caf9e9cd
abibring <omniparkingwebhook@gmail.com>
2022-04-27 16:35:47 -0400

    changing file path for logo to relative to use image in repo (Testing)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+/*jshint esversion: 8 */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  serverRuntimeConfig: {
+    PROJECT_ROOT: __dirname
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
