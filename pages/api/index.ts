6ccea805162d49c2f2baf38ec0f3b931c7cd1abe
abibring <omniparkingwebhook@gmail.com>
2022-09-13 20:57:48 -0400

    everything working perfectly. Getting icon locally instead of from s3 bucket now. turning off webhook

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -35,7 +35,7 @@ const transporter = nodemailer.createTransport({ auth: { user, pass }, host, por
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   try {
     const { body, headers, method } = req;
-    // return res.status(201).send({ message: 'Webhook turned off!' }); // REMOVE WHEN READY FOR PROD
+    return res.status(201).send({ message: 'Webhook turned off!' }); // REMOVE WHEN READY FOR PROD
 
     // console.log('fileContents:', iconBase64)
     if (method === 'POST') {
@@ -64,10 +64,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       // If no start or end times from booking, event failed
       if (!start_time || !end_time) { return res.status(201).send({ message: h.missingTimeInfoMessage }); }
 
-      // const startTimeFormatted: string = h.formatDate(start_time);
-      // const endTimeFormatted: string = h.formatDate(end_time);
-      const startTimeFormatted = '13.09.202207:00:00';
-      const endTimeFormatted = '16.09.202223:00:00';
+      const startTimeFormatted: string = h.formatDate(start_time);
+      const endTimeFormatted: string = h.formatDate(end_time);
+      // const startTimeFormatted = '13.09.202207:00:00';
+      // const endTimeFormatted = '16.09.202223:00:00';
 
       // Generate date in MM/DD/YYYY format for email
       const createdAt: string = h.formatDateTimeAsString(created_at);
