b366fc8a348d72f62122aff8a6655eae36c545d7
omniparking <omniparkingwebhook@gmail.com>
2024-01-17 17:41:45 -0500

    styling page where you view QR code

--- pages/view/qr.tsx
+++ pages/view/qr.tsx
@@ -1,6 +1,37 @@
 import { useEffect, useState } from "react";
 import QRCode from "qrcode";
+import styled from "styled-components";
 import moment from "moment";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  margin-left: 20px;
+  gap: 12px;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Paragraph = styled.p`
+  font-weight: 600;
+  display: inline-block;
+  font-size: 18px;
+  margin: 0;
+`;
+
+const Span = styled.span`
+  font-size: 20px;
+`;
+
+const SpanBlock = styled.span`
+  display: block;
+`;
+
+const H1 = styled.h1`
+  margin: 24px 0 12px 0;
+`;
 
 const QRPage = ({ startTime, endTime, qrcodeData }) => {
   const [qrDataURL, setQRDataURL] = useState("");
@@ -13,12 +44,19 @@ const QRPage = ({ startTime, endTime, qrcodeData }) => {
   }, [qrcodeData]);
 
   return (
-    <div>
-      <h1>Omni Airport Booking:</h1>
-      <p>Start Time: {moment(startTime).format("MMMM Do YYYY, h:mm a")}</p>
-      <p>End Time: {moment(endTime).format("MMMM Do YYYY, h:mm a")}</p>
+    <Container>
+      <H1>Omni Airport Parking</H1>
+      <SpanBlock>
+        <Paragraph>Start Time:</Paragraph>{" "}
+        <Span>{moment(startTime).format("MM/DD/YY h:mm a")}</Span>
+      </SpanBlock>
+
+      <SpanBlock>
+        <Paragraph>End Time:</Paragraph>{" "}
+        <Span>{moment(endTime).format("MM/DD/YY h:mm a")}</Span>
+      </SpanBlock>
       {qrDataURL && <img src={qrDataURL} alt="QR Code" />}
-    </div>
+    </Container>
   );
 };
 
