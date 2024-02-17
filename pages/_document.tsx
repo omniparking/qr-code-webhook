02274869065707d8e4b64774ef05b82af762c3f7
omniparking <omniparkingwebhook@gmail.com>
2024-02-17 08:29:04 -0500

    adding in new font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,12 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
